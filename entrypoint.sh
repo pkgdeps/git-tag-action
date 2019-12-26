7dc93f3fefd7b9bf9d40af6d34131f95503871ad
azu <azuciao@gmail.com>
2019-12-26 16:29:33 +0900

    fix cd

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
- 
+
+cd "$GITHUB_WORKSPACE"
 # check this version is enable to release or not
 npx can-npm-publish --verbose
 if [ $? -eq 1 ] ; then
@@ -8,17 +9,17 @@ fi
  
 # get current version from package.json
 VERSION=$(cat package.json | jq .version)
-GIT_TAG_NAME=${git_tag_prefix}${VERSION}
+GIT_TAG_NAME=${INPUT_GIT_TAG_PREFIX}${VERSION}
 echo "add new tag to GitHub: ${GIT_TAG_NAME}"
  
 # Add tag to GitHub
-API_URL="https://api.github.com/repos/${github_repo}/git/refs"
+API_URL="https://api.github.com/repos/${INPUT_GITHUB_REPO}/git/refs"
  
 curl -s -X POST $API_URL \
-  -H "Authorization: token $github_token" \
+  -H "Authorization: token ${INPUT_GITHUB_TOKEN}" \
   -d @- << EOS
 {
   "ref": "refs/tags/${GIT_TAG_NAME}",
-  "sha": "${git_commit_sha}"
+  "sha": "${INPUT_GIT_COMMIT_SHA}"
 }
 EOS
